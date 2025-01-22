b0af2a7a1033d14f4e19f9c77e421e7ce89fe8ee
Ghizlane Ghaya <gghaya@e2r8p15.1337.ma>
2025-01-22 17:20:17 +0100

    v2

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 openssl genrsa -out /etc/certs/ca.key 2048
 openssl req -new -x509 -key /etc/certs/ca.key -out /etc/certs/ca.crt -days 365 -batch
-openssl req -new -nodes -out /etc/certs/prometheus.csr -newkey rsa:4096 -keyout /etc/certs/prometheus.key -subj '/CN=127.0.0.1'
+openssl req -new -nodes -out /etc/certs/prometheus.csr -newkey rsa:4096 -keyout /etc/certs/prometheus.key -subj '/CN=host.docker.internal'
 # openssl req -new -nodes -out /etc/certs/prometheus.csr -newkey rsa:4096 -keyout /etc/certs/prometheus.key -subj "/CN=127.0.0.1" -config <(echo -e "[req]\ndistinguished_name = req_distinguished_name\n[req_distinguished_name]\n[alt_names]\n
 # DNS.1 = prometheus.example.com\nIP.1 = 127.0.0.1")
 openssl x509 -req -in /etc/certs/prometheus.csr -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -out /etc/certs/prometheus.crt -days 730 -sha256
